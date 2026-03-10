2039feb46346f8b2b5e0ababd645bcfe89eb8cc2
DigitalPaaji <147261989+kashishpahuja@users.noreply.github.com>
2026-03-10 16:54:52 +0530

    rt

---
+++ frontend/src/app/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+
+function page() {
+  return (
+    <div>
+      <Navbar/>
+      <Banner/>
+    </div>
+  )
+}
+
+export default page
